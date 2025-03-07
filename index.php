5af56356f7228ff29426292abdbb11a270da8481
VialsShiny <150128459+VialsShiny@users.noreply.github.com>
2025-03-07 14:35:48 +0100

    First Version

--- index.php
+++ index.php
@@ -5,11 +5,57 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Loading Page Done</title>
+  <style>
+    .loading-screen {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #000;
+      /* Optionnel, pour un fond sombre */
+    }
+
+    .loading-screen::before,
+    .loading-screen::after,
+    .loading-screen span {
+      content: '';
+      width: 10px;
+      height: 10px;
+      background-color: white;
+      border-radius: 50%;
+      margin: 0 5px;
+      animation: jump 0.6s ease-in-out infinite;
+    }
+
+    .loading-screen::before {
+      animation-delay: 0s;
+    }
+
+    .loading-screen span {
+      animation-delay: 0.2s;
+    }
+
+    .loading-screen::after {
+      animation-delay: 0.4s;
+    }
+
+    @keyframes jump {
+
+      0%,
+      100% {
+        transform: translateY(0);
+      }
+
+      50% {
+        transform: translateY(-15px);
+      }
+    }
+  </style>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-  
+
 <body class="bg-gradient-to-r from-black to-purple-600">
-  <div class="loading-screen"></div>
+  <div class="loading-screen"><span></span></div>
   <div class="content h-screen flex flex-col items-center justify-center">
     <h1 class="text-white text-4xl font-bold mb-8">
       Loading Page Done
--- loading.js
+++ loading.js
@@ -1,12 +1,26 @@
 const xhr = new XMLHttpRequest();
 
+function createLoadingPage() {
+  const loadingPage = document.querySelector('.loading-screen');
+  loadingPage.style.display = 'auto';
+  loadingPage.style.visibility = 'visible';
+}
+
+function cutLoadingPage() {
+  const loadingPage = document.querySelector('.loading-screen');
+  loadingPage.style.display = 'none';
+  loadingPage.style.visibility = 'hidden';
+}
+
 xhr.open("GET", "https://api.thecatapi.com/v1/images/search", true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 xhr.onload = function () {
+  createLoadingPage();
   if (xhr.status >= 200 && xhr.status < 300) {
     const response = JSON.parse(xhr.responseText);
     document.getElementById('imageContainer').innerHTML = `<img src="${response[0].url}" alt="Cat Image" class="w-96 h-96 object-cover object-center rounded-[2vw]">`;
+    cutLoadingPage();
   } else {
     document.getElementById('imageContainer').innerHTML = "Erreur lors de la récupération des données.";
   }
